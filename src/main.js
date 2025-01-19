d6e0731781d92c14fa91157f58f8721465f21846
maryna33309 <68459246+marusya2299@users.noreply.github.com>
2025-01-19 12:10:29 +0100

    nav menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-import './style.css'
+import './style.css';
+
+const button = document.querySelector('.box-navigation-icon');
+const menu = document.querySelector('.menu-navigation');
+const iconClose = document.querySelector('.icon-close');
+const header = document.querySelector('.header');
+const elNavigation = header.querySelectorAll('.element-navigation');
+
+button.addEventListener('click', () => {
+    menu.classList.toggle('active');
+
+})
+
+iconClose.addEventListener('click', ()=>{
+    menu.classList.toggle('active');
+})
+
+elNavigation.forEach(navItem =>{
+    navItem.addEventListener('click', ()=>{
+        menu.classList.toggle('active');
+    });
+})
+
+
